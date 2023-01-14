5408672c55113cd1c739bd8bb5777a348cb266c4
durkes <durkes@users.noreply.github.com>
2023-01-14 10:09:30 -0600

    Set page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react';
 import { HashRouter } from 'react-router-dom';
 import BaseLayout from './components/BaseLayout';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Auto Classifieds - Buy & Sell Cars';
+  });
+
   return (
     <HashRouter>
       <BaseLayout />
