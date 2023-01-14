0c53781c4a8c57c5ac64e1d85a1a40caac6365ec
durkes <durkes@users.noreply.github.com>
2023-01-14 17:16:15 -0600

    Added NewUser form

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -28,12 +28,33 @@ export default function Login() {
                     </div>
 
                     <div class="grid gap-y-3">
-                        <input type="email" class="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400" placeholder="email@example.com" />
+                        <input type="email" required class="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400" placeholder="email@example.com" />
                         <button class="flex items-center justify-center gap-x-2 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-300 transition hover:text-purple-400">
                             <svg style={{ color: 'rgb(203, 213, 225)' }} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                                 <path fill="#cbd5e1" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
                             </svg>
                             Sign in with Email
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+function NewUser() {
+    return (
+        <div class="z-60 fixed top-0 left-0 overflow-auto grid h-screen w-screen place-items-center bg-slate-800 px-4 text-sm font-medium">
+            <div class="w-full max-w-sm rounded-lg bg-slate-700/30 shadow">
+                <form class="p-4 md:p-5 lg:p-6">
+                    <div class="mb-4 text-center text-slate-500">Let's create an account for you! <span className="text-xl leading-none">🖐️</span></div>
+
+                    <div class="grid gap-y-3">
+                        <input type="email" required value="email@example.com" class="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400" />
+                        <input type="password" required placeholder="Password" class="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400" />
+                        <input type="password" required placeholder="Password (again)" class="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400" />
+                        <button class="flex items-center justify-center gap-x-2 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-300 transition hover:text-purple-400">
+                            Create Account
                         </button>
                     </div>
                 </form>
