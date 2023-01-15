02a22e794c17454709e1ad0504b744d691772b8e
durkes <durkes@users.noreply.github.com>
2023-01-15 01:47:44 -0600

    Remove test code

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -4,10 +4,6 @@
 export default function Login() {
     const serverUrl = window.location.protocol + '//' + window.location.host;
     const redirectUrl = serverUrl + '/oauth-redirect.html';
-
-    // get links from /oauth-test.html
-    // TODO need to get these from react query which will get them from PBDB
-    // TODO stor the state in localstorage when onclick on a provider
     const twitterUrl = '/oauth/twitter?redirect_uri=' + redirectUrl;
     const googleUrl = '/oauth/google?redirect_uri=' + redirectUrl;
 
@@ -16,7 +12,7 @@ export default function Login() {
             <div class="w-full max-w-sm rounded-lg bg-slate-700/30 shadow">
                 <form class="p-4 md:p-5 lg:p-6">
                     <div class="grid gap-y-3">
-                        <a href={twitterUrl} onClick={() => alert('clicked')} class="flex items-center justify-center gap-x-2 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-300 transition hover:text-purple-400">
+                        <a href={twitterUrl} class="flex items-center justify-center gap-x-2 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-300 transition hover:text-purple-400">
                             <svg style={{ color: 'rgb(203, 213, 225)' }} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
                                 <path fill="#cbd5e1" d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
                             </svg>
