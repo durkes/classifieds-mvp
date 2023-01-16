c1b51823491a5db6675f4cff9e0776ccb51917d3
durkes <durkes@users.noreply.github.com>
2023-01-15 22:33:35 -0600

    Fix class to className

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -1,8 +1,8 @@
 export default function LoadingOverlay() {
     return (
-        <div class="z-90 fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-40">
-            <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
-                <div class="border-transparent border-t-white border-4 h-16 w-16 border-solid rounded-full animate-spin-fast"></div>
+        <div className="z-90 fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-40">
+            <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
+                <div className="border-transparent border-t-white border-4 h-16 w-16 border-solid rounded-full animate-spin-fast"></div>
             </div>
         </div>
     );
