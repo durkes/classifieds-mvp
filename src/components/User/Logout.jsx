37e1a7e9ad9056045d2255720d0bf62539f850b5
durkes <durkes@users.noreply.github.com>
2023-01-17 10:51:01 -0600

    Implemented logout function

--- src/components/User/Logout.jsx
+++ src/components/User/Logout.jsx
@@ -1,8 +1,45 @@
+import { useEffect, useContext } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+import { Navigate } from 'react-router-dom';
+import fetchHelper from '../../assets/fetch-helper';
+import { getCookie } from '../../assets/browser-cookies';
+import LoadingOverlay from '../LoadingOverlay';
+import SessionContext from '../../context/SessionContext';
+
 export default function Logout() {
-    return (
-        <div>
-            <p>This is the logout page</p>
-            <a href="/user/login">To the login page</a>
-        </div>
-    );
+    const { sessionData, setSessionData } = useContext(SessionContext);
+
+    const queryClient = useQueryClient();
+    const queryKey = 'logout';
+
+    const { isSuccess, isError, /*data, error,*/ refetch } = useQuery(queryKey, () =>
+        fetchHelper('get', '/v1/logout', {}),
+        { enabled: false }); // disable automatic re/fetch (to prevent infinite loop w/ removeQueries)
+
+    useEffect(() => {
+        refetch(); // fire only once by passing dependencies[] to useEffect (below) that do not change
+
+        return function onUnmount() {
+            queryClient.removeQueries(queryKey, { exact: true }); // remove the query cache on unmount
+        };
+    }, [refetch, queryClient]); // dependencies[] to prevent useEffect from firing every render
+
+    useEffect(() => {
+        if (isSuccess) {
+            setSessionData({
+                ...sessionData,
+                isLoggedIn: getCookie('isLoggedIn'),
+                userEmail: getCookie('userEmail')
+            });
+        }
+    }, [isSuccess]);
+
+    if (isError) {
+        return <Navigate replace to="/user/login" />;
+    }
+    if (isSuccess) {
+        return <Navigate replace to="/" />;
+    }
+
+    return <LoadingOverlay />;
 }
