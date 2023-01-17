9c4e18a099dc4312f8aec3e528577ee1a1c7e0a1
durkes <durkes@users.noreply.github.com>
2023-01-17 17:52:04 -0600

    Create ListingItem.jsx

---
+++ src/components/Listings/ListingItem.jsx
@@ -0,0 +1,12 @@
+export default function ListingItem() {
+    return (
+        <div class="grid grid-cols-4 gap-4">
+            <div className="bg-slate-600">01</div>
+            <div className="bg-slate-600">01</div>
+            <div className="bg-slate-600">01</div>
+            <div className="bg-slate-600">01</div>
+            <div className="bg-slate-600">01</div>
+            <div className="bg-slate-600">01</div>
+        </div>
+    );
+}
