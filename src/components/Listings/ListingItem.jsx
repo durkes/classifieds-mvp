aee4befe66f25964c24e876a7da1ffe422fdacc3
durkes <durkes@users.noreply.github.com>
2023-01-18 12:09:06 -0600

    Add logic to display real data

--- src/components/Listings/ListingItem.jsx
+++ src/components/Listings/ListingItem.jsx
@@ -1,42 +1,42 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import fetchHelper from '../../assets/fetch-helper';
+import LoadingOverlay from '../LoadingOverlay';
+
 export default function ListingItem() {
+    const { id } = useParams();
+    const { isSuccess, isError, data, error } = useQuery(['item', id], () =>
+        fetchHelper('post', '/v1/listings/item', { id: id }));
+
+    if (!isSuccess) {
+        return <LoadingOverlay />;
+    }
+
     return (
         <div className="grid md:grid-cols-7 gap-6">
             <aside className="md:col-span-3">
-                <img className="rounded-lg" src="https://images.craigslist.org/00Y0Y_1tuTErtSfYQ_0uY0ne_600x450.jpg" alt="Vehicle for sale" />
+                <img className="rounded-lg" src={'/img/' + id + '.jpg'} alt={data.headline} />
             </aside>
             <section className="md:col-span-4 prose prose-slate">
-                <h2 className="mb-2">2013 Nissan Altima, black exterior, tan leather interior</h2>
+                <h2 className="mb-2">{data.year} {data.headline}</h2>
                 <div>
                     <span className="bg-indigo-100 text-indigo-800 font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-indigo-900 dark:text-indigo-300">
-                        Car
+                        {data.type}
                     </span>
                     <span className="pr-1 bg-indigo-100 text-indigo-800 font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-indigo-900 dark:text-indigo-300">
-                        60,543<span className="ml-0.5">🕛</span>
+                        {data.mileage}<span className="ml-0.5">🕛</span>
                     </span>
                     <span className="bg-indigo-100 text-indigo-800 font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-indigo-900 dark:text-indigo-300">
-                        $7,900
+                        {'$' + data.price}
                     </span>
                     <button className="float-right text-xs tracking-wide uppercase">Favorite <span className="text-lg">◻️⭐️</span></button>
                 </div>
-                <p>
-                    selling my 2013 Nissan Maxima SV, clean title, 194000 miles on it. Transmission recently replaced with a brand new one.
-
-                    According to KBB, fair condition low price is $4,172. condition is better than just "fair", only marked as fair for the few issues mentioned below.
-
-                    priced to sell.
-
-                    6 cylinder engine.
-                    Sunroof.
-                    Backup Camera.
-                    Premium bose sound system.
-                    Light beige leather seats.
-
-                    The bad:
-                    -Paint scratches/scrape on the rear bumper right side (see 6th picture)
-                    -Leak in power steering fluid (Power steering works but you have to keep toping it up every few days)
-                    -Needs O2 sensor (to pass inspection, but runs and drives fine)
-                </p>
-                <a href="/">Contact this seller</a> <button>🗑️<span className="underline">Delete this listing</span></button>
+                <p>{data.description}</p>
+                {data.isOwner ?
+                    <button>🗑️<span className="underline">Delete this listing</span></button> :
+                    <button><span className="underline">Contact this seller</span></button>
+                }
             </section>
         </div >
     );
