5f87b1c3de50d968e2dff485af49503dc9474107
durkes <durkes@users.noreply.github.com>
2023-01-18 14:40:52 -0600

    Add route for ListingEdit

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import { MyAccount, UserCreate, Login, LoginEmail, OAuthCallback, Logout } from './User';
-import { Listings, ListingItem, MyListings, MyFavorites, ListingCreate } from './Listings';
+import { Listings, ListingItem, MyListings, MyFavorites, ListingCreate, ListingEdit } from './Listings';
 
 export default function BaseLayout() {
     return (
@@ -13,6 +13,7 @@ export default function BaseLayout() {
                     <Route path="/listings/find" element={<Listings />} />
                     <Route path="/listings/item/:id" element={<ListingItem />} />
                     <Route path="/listings/new" element={<ListingCreate />} />
+                    <Route path="/listings/edit/:id" element={<ListingEdit />} />
                     <Route path="/listings/mine" element={<MyListings />} />
                     <Route path="/listings/saved" element={<MyFavorites />} />
                     <Route path="/user/account" element={<MyAccount />} />
