a9fe8803898cd00fdcf56cebfacb900b8a195673
durkes <durkes@users.noreply.github.com>
2023-01-18 14:40:34 -0600

    Add ListingEdit export

--- src/components/Listings/index.jsx
+++ src/components/Listings/index.jsx
@@ -3,5 +3,6 @@ import ListingItem from './ListingItem';
 import MyListings from './MyListings';
 import MyFavorites from './MyFavorites';
 import ListingCreate from './ListingCreate';
+import ListingEdit from './ListingEdit';
 
-export { Listings, ListingItem, MyListings, MyFavorites, ListingCreate };
+export { Listings, ListingItem, MyListings, MyFavorites, ListingCreate, ListingEdit };
