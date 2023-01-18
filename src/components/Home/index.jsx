f3b52193adb76b97aae647871e1ba0b8407d7c22
durkes <durkes@users.noreply.github.com>
2023-01-18 10:16:58 -0600

    Home redirects to Listings

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,5 @@
+import { Navigate } from 'react-router-dom';
+
 export default function Home() {
-    return (
-        <div>
-            <p>This is the home page. In the future, this page will feature new listings, CTAs to other pages, etc.</p>
-            <a href="/user/login">Go to login</a>
-        </div>
-    );
+    return <Navigate replace to="/listings/find" />;
 }
