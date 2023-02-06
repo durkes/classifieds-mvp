e72f256d63f45b2f75ed73af1d1ca761015518b3
durkes <durkes@users.noreply.github.com>
2023-02-05 18:04:56 -0600

    Add proxy paths for dev mode

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  server: {
+    proxy: { // when using npm dev script, must also run server
+      '/v1': 'http://localhost',
+      '/img': 'http://localhost'
+    }
+  }
 });
