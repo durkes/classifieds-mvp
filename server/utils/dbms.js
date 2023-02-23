590d2f297a73073db39b413777c245dcc1c0c282
durkes <durkes@users.noreply.github.com>
2023-02-23 10:07:16 -0600

    Add dbms auth debug

--- server/utils/dbms.js
+++ server/utils/dbms.js
@@ -8,6 +8,9 @@ const adminPassword = adminUsername;
 export const pbAdmin = new PocketBase('http://127.0.0.1:8090');
 const adminData = await pbAdmin.admins.authWithPassword(adminUsername, adminPassword);
 console.debug(adminData); // do not expose token in production
+console.debug(pbAdmin.authStore.isValid);
+console.debug(pbAdmin.authStore.token);
+console.debug(pbAdmin.authStore.model.id);
 
 // need separate instances for user and admin
 // otherwise admin privileges are lost after authenticating a user on a single/shared instance
